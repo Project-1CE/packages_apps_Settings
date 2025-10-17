aosp-mirror/platform_packages_apps_settings

commit 15ca95a31b51682f868e486e9f07c64d785c3f4b

Migrate to TwoStatePreference
SwitchPreference and SwitchPreferenceCompat are both TwoStatePreference.

Using TwoStatePreference in Java will helps migration in the future.

Bug: 306771414
Test: manual - check Settings pages
Change-Id: I84e1d7b09451106797c2b23d127855c6976678ca

src/com/android/settings/development/ColorModePreference.java
@@ -24,14 +24,14 @@ import android.os.Looper;
 import android.util.AttributeSet;
 import android.view.Display;
 
-import androidx.preference.SwitchPreference;
+import androidx.preference.TwoStatePreference;
 
 import com.android.settingslib.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ColorModePreference extends SwitchPreference implements DisplayListener {
+public class ColorModePreference extends TwoStatePreference implements DisplayListener {
 
     private DisplayManager mDisplayManager;
     private Display mDisplay;

Project-1CE/packages_apps_Settings

commit 3aca1d7bf910c77cf8016d5ecb66588aca541644

Settings: Fix development color mode preference
This is a sed gone wrong.
It was unintentionally changed in 15ca95a31b51682f868e486e9f07c64d785c3f4b
which broke functionality.

Change it to a SwitchPreferenceCompat instead of changing it back
to a SwitchPreference, as all the other preferences in the
development section are one as well.

Change-Id: Id276d3b67c103b83ba01a69ff2991246c69efc69
Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

src/com/android/settings/development/ColorModePreference.java
@@ -24,14 +24,14 @@ import android.os.Looper;
 import android.util.AttributeSet;
 import android.view.Display;
 
-import androidx.preference.TwoStatePreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settingslib.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ColorModePreference extends TwoStatePreference implements DisplayListener {
+public class ColorModePreference extends SwitchPreferenceCompat implements DisplayListener {
 
     private DisplayManager mDisplayManager;
     private Display mDisplay;